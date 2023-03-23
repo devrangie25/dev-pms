668b8503a1d9e92e79b031b7a1879d3cfdd6284f
Dev Rangie <devrangie25@gmail.com>
2023-03-23 22:45:20 +0800

    Setup Supabase in App

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { loadFonts } from './plugins/webfontloader'
 import router from './route/index'
 import { registerLayouts } from './layouts/register';
 import { registerComponents } from './components/register';
+import { supabase } from './lib/supabaseClient';
 
 loadFonts()
 
@@ -15,5 +16,7 @@ app.use(router)
 
 registerLayouts(app);
 registerComponents(app);
+
+app.provide('supabase', supabase)
 
 app.mount('#app')
